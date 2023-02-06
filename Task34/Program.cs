0b1355072b5ee36e4f087e51580639db991c68d8
Sarmaon <patratiyaa@gmail.com>
2023-02-06 17:41:47 +0800

    Tasks34&36

--- Task34/Program.cs
+++ Task34/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+//Напишите программу, которая покажет количество чётных чисел в массиве.
+
+Console.WriteLine("Введите размер массива");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[size];
+
+ArrayRandomNumbers(numbers);
+Console.WriteLine("Полученный массив: ");
+//PrintArray(numbers);
+int count = 0;
+
+for (int a = 0; a < numbers.Length; a++)
+    if (numbers[a] % 2 == 0)
+        count++;
+
+Console.WriteLine($"Всего {numbers.Length} чисел, {count} из них четные");
+
+void ArrayRandomNumbers(int[] numbers)
+{
+    for (int b = 0; b < numbers.Length; b++)
+    {
+        numbers[b] = new Random().Next(100, 999);
+    }
+}
+{
+    Console.Write("[ ");
+    for (int b = 0; b < numbers.Length; b++)
+    {
+        Console.Write(numbers[b] + " ");
+    }
+    Console.Write("]");
+    Console.WriteLine();
+}
---
+++ Task36/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 36. Задайте одномерный массив, заполненный случайными числами. 
+//Найдите сумму элементов, стоящих на нечётных позициях.
+
+
+        int[] Array = { 4, 8, 5, 4 };
+        int result = 0;
+
+        for (int a = 0; a < Array.Length; a++)
+        {
+            if (Array[a] % 2 == 1)
+            {
+                result = result + Array[a];
+            }
+        }
+    Console.WriteLine(result);
